b16519778f9d3890312917520c5314f4c0b94af2
Maria Luiza <maria.lmm.975@gmail.com>
2022-05-11 22:02:29 -0300

    chore: adjust code for deploy on heroku

--- index.js
+++ index.js
@@ -17,10 +17,8 @@ app.use(cors());
 app.use(userRouter);
 app.use(transactionsRouter);
 
-
-
-
+const PORT = process.env.PORT || 5000;
 
 app.listen(5000, () =>
-    console.log(chalk.green("Server is running on port 5000"))
+    console.log(chalk.green(`Server is running on port ${PORT}`))
 );
